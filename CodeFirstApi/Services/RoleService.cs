f63e5ac7214890d47bc3062838bd381f8308f037
Kishore Reddy <3920322+kishoreredy@users.noreply.github.com>
2024-07-28 03:56:58 +0530

    Role related changes added

---
+++ CodeFirstApi/Services/RoleService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CodeFirstApi.Services
+{
+    public interface IRoleService
+    {
+        Task<bool> AddRole(string role);
+    }
+
+    public class RoleService(RoleManager<IdentityRole> roleManager, ILogger<RoleService> logger) : IRoleService
+    {
+        private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+        private readonly ILogger<RoleService> _logger = logger;
+
+        public async Task<bool> AddRole(string role)
+        {
+            if (await _roleManager.RoleExistsAsync(role))
+            {
+                _logger.LogError("Role already exists");
+                return false;
+            }
+
+            var identityRole = new IdentityRole(role);
+            return (await _roleManager.CreateAsync(identityRole)).Succeeded;
+        }
+    }
+}
